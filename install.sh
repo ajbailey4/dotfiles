77655cd2b36ef843d0f301848bb8d1ece6149150
Aydan <aydan.bailey@gmail.com>
2025-08-01 14:25:44 -0500

    Initial commit with install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOTFILES_DIR="$HOME/dotfiles"
+
+FILES=(
+  ".vimrc"
+  ".bashrc"
+  ".gitconfig"
+  ".tmux.conf"
+)
+
+for file in "${FILES[@]}"; do
+    src="$DOTFILES_DIR/$file"
+    dst="$HOME/$file"
+
+    if [ ! -e "$src" ]; then
+      echo "Warning: source file $src does not exist. Skipping."
+      continue
+    fi
+
+    echo "Linking $dst → $src"
+    ln -sf "$src" "$dst"
+done
+
